51779e4544b1ddd42376721a62f355b39237ce20
alessandro <ale_hagy@hotmail.com>
2023-09-28 17:25:59 -0300

    primeiro comitt

---
+++ script.js
@@ -0,0 +1,8 @@
+function conecta() {
+    console.log ("Conexão feita com sucesso!")
+    console.log ("Conexão feita com sucesso!")
+    console.log ("Conexão feita com sucesso!")
+}
+
+conecta ();
+//imprimira 3x "Conexão feita com sucesso!"
